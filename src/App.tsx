74e4e917d5db4e8667f85bb4c79472e5bc6b209e
uinus-wongil <uinus.kwk@uinus.co.kr>
2024-10-28 12:52:55 +0900

    feat: router, pwa 빌드 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
 import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
 
+import Router from '@@router';
 import { store } from '@@store';
 
 function App() {
-  return <Provider store={store}></Provider>;
+  return (
+    <BrowserRouter>
+      <Provider store={store}>
+        <Router />
+      </Provider>
+    </BrowserRouter>
+  );
 }
 
 export default App;
