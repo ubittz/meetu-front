ab3ef45da9c9e7176e6e2cca17a0e2bfb61c88f3
uinus-wongil <uinus.kwk@uinus.co.kr>
2024-12-10 16:24:23 +0900

    feat: remove restrict mode;

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,11 @@
-import { StrictMode } from 'react';
-
 import { createRoot } from 'react-dom/client';
 
 import App from '@@App';
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
-);
+// Strict Mode에서 render가 두번 되는 버그가 있어서 strictmode 제거
+// createRoot(document.getElementById('root')!).render(
+//   <StrictMode>
+//     <App />
+//   </StrictMode>
+// );
+createRoot(document.getElementById('root')!).render(<App />);
